2f2b40a9b34d41986e931f096b34a62ef05ff530
Ian Fund <ianfund117@gmail.com>
2022-11-08 00:01:24 -0600

    Started

---
+++ src/Components/HomePage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import NavigationBar from "./NavigationBar";
+import logo from "../media/FULogo.png";
+export default function HomePage() {
+  return (
+    <div>
+      <NavigationBar />
+      Website
+    </div>
+  );
+}
---
+++ src/Components/NavigationBar.js
@@ -0,0 +1,45 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import logo from "../media/FULogo.png";
+
+function NavigationBar() {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">
+          <img
+            src={logo}
+            width="50"
+            height="50"
+            className="d-inline-block align-top"
+            alt="FU logo"
+          />
+          Focus Up
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">About Us</Nav.Link>
+            <Nav.Link href="#link"></Nav.Link>
+            <NavDropdown title="Guides" id="basic-nav-dropdown"></NavDropdown>
+            <NavDropdown title="Kill videos" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavigationBar;
